9bce77f2dd1d892e5e8979d4706ccf4dda02f0fb
Florian Chytilek <a1309651@unet.univie.ac.at>
2015-04-20 17:02:13 +0200

    Klassendiagramm übernahme

--- festivals_bl/src/festivals_bl/Stand.java
+++ festivals_bl/src/festivals_bl/Stand.java
@@ -1,5 +1,35 @@
 package festivals_bl;
 
-public class Stand {
+import java.util.ArrayList;
 
+public class Stand {
+	
+	private int vid;
+	private int standnummer;
+	private Mitarbeiter standleiter;
+	private Mitarbeiter stellvertreter;
+	private ArrayList<Mitarbeiter> mitarbeiterListe;
+	VeranstaltungsVerwaltungSingleton vv = VeranstaltungsVerwaltungSingleton.getInstance();
+	
+	public void standleiterAendern(Mitarbeiter maNeu) throws Exception{
+		Veranstaltung veranstaltung = vv.getVeranstaltungById(vid);
+		veranstaltung.statusAendern(maNeu, "standleiter");
+		standleiter = maNeu;
+	}
+	
+	public void stellvertreterAendern(Mitarbeiter maNeu) throws Exception{
+		Veranstaltung veranstaltung = vv.getVeranstaltungById(vid);
+		veranstaltung.statusAendern(maNeu, "stellvertreter");
+		stellvertreter = maNeu;
+	}
+	
+	public void mitarbeiterHinzufuegen(Mitarbeiter ma){ 
+		if(!mitarbeiterListe.contains(ma))
+			mitarbeiterListe.add(ma);
+	}
+	
+	public void mitarbeiterLoeschen(Mitarbeiter ma){
+		if(mitarbeiterListe.contains(ma))
+			mitarbeiterListe.remove(ma);
+	}
 }
