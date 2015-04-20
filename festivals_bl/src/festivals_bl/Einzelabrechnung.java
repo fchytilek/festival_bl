2efd470aa8a6d89d50f5269a66ec7d1bb9282d27
Wolfgang Erler <a1309652@unet.univie.ac.at>
2015-04-20 15:44:10 +0200

    asdasd

--- festivals_bl/src/festivals_bl/Einzelabrechnung.java
+++ festivals_bl/src/festivals_bl/Einzelabrechnung.java
@@ -1,5 +1,28 @@
 package festivals_bl;
 
-public class Einzelabrechnung {
+import java.util.ArrayList;
 
+public class Einzelabrechnung {
+	private Mitarbeiter mitarbeiter;
+	private int vid;
+	private ArrayList<Arbeit> arbeitsliste;
+	private double anreisekosten;
+	private String anreiseKommentar;
+	private boolean aktiv;
+	private boolean anreiseBestaetigt;
+	public void arbeitHinzufuegen(Arbeit arb){
+		
+	}
+	public void arbeitLoeschen(Arbeit arb){
+		
+	}
+	public void anreisekostenAngeben(double kosten, String kommentar){
+		
+	}
+	public void anreisekostenBestaetigen(){
+		
+	}
+	public void anreisekostenKorrigieren(double kosten, String kommentar){
+		
+	}
 }
